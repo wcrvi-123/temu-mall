7f7262e772330cb0c2a6b993e2fc5e1d7eeb5021
wcrvi-123 <thhehetgfrhfg@gmail.com>
2025-08-08 11:34:39 +0800

    Add files via upload

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+export default function Home() {
+  return (
+    <>
+      <Head><title>Temu Mall</title></Head>
+      <main>
+        <h1>Welcome to Temu Mall</h1>
+        <p>This is the homepage.</p>
+      </main>
+    </>
+  )
+}
