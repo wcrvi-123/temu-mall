def579205528a60f75f6c832ca681744bb9a5761
wcrvi-123 <thhehetgfrhfg@gmail.com>
2025-08-08 14:42:25 +0800

    Update Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ background: '#333', color: '#fff', padding: '10px', marginTop: '20px' }}>
+      <p>版权所有 © Temu Mall 2025</p>
+    </footer>
+  );
+}
